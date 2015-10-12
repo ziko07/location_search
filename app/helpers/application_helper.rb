00e024107689c409f27fce6896fad2f090c594f6
nazrul <nazrulku07@gmail.com>
2015-10-12 17:51:07 +0600

    remove map from second form and search business at second form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_tab(controller, action)
+    if params[:controller] == controller && action.include?(params[:action])
+      'active'
+    else
+      ''
+    end
+  end
 end
